de9e08202190edf4fdc92d151817118d4fbf5334
yagrariksa <luckyrama0@gmail.com>
2022-06-22 19:42:42 +0700

    refactor ReviewTest

--- tests/Feature/ReviewTest.php
+++ tests/Feature/ReviewTest.php
@@ -9,11 +9,6 @@ use Tests\TestCase;
 
 class ReviewTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_get_all_review()
     {
         $response = $this->get('/api/review');
@@ -22,7 +17,79 @@ class ReviewTest extends TestCase
             ->assertStatus(200)
             ->assertJson(
                 fn (AssertableJson $json) =>
-                $json->hasAll(['data', 'message'])
+                $json
+                    ->has('message')
+                    ->whereAllType([
+                        'message' => 'string',
+                        'data' => 'array'
+                    ])
+                    ->has(
+                        'data',
+                        100,
+                        fn ($review) =>
+                        $review
+                            ->hasAll([
+                                'desc', 'rating', 'price',
+                                'FDA', 'created_at', 'updated_at',
+                                'food', 'user', 'url'
+                            ])
+                            ->whereAllType([
+                                'desc' => 'string',
+                                'rating' => 'integer',
+                                'price' => 'integer',
+                                'FDA' => 'string',
+                                'created_at' => 'string',
+                                'updated_at' => 'string',
+                                'food' => 'array',
+                                'user' => 'array',
+                                'url' => 'array',
+                            ])
+                            ->has(
+                                'food',
+                                fn ($food) =>
+                                $food
+                                    ->has('name')
+                                    ->whereType('name', 'string')
+                                    ->has(
+                                        'restaurant',
+                                        fn ($restaurant) =>
+                                        $restaurant
+                                            ->hasAll([
+                                                'name', 'location', 'district'
+                                            ])
+                                            ->whereAllType([
+                                                'name' => 'string',
+                                                'location' => 'string',
+                                                'district' => 'string',
+                                            ])
+                                    )
+
+                            )
+                            ->has(
+                                'user',
+                                fn ($user) =>
+                                $user
+                                    ->hasAll([
+                                        'name', 'email'
+                                    ])
+                                    ->whereAllType([
+                                        'name' => 'string',
+                                        'email' => 'string'
+                                    ])
+                            )
+                            ->has(
+                                'url',
+                                fn ($url) =>
+                                $url
+                                    ->hasAll([
+                                        'all', 'details'
+                                    ])
+                                    ->whereAllType([
+                                        'all' => 'string',
+                                        'all' => 'string',
+                                    ])
+                            )
+                    )
             );
     }
 }
